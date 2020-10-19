cfda8b4dcf0a8ff77dae17e6c2aa1e3dba04a06c
danielsousaio <daniel.s@crowdbotics.com>
2020-10-19 16:06:38 +0100

    Update screen

--- src/features/BlankScreen022/screens/blank.js
+++ src/features/BlankScreen022/screens/blank.js
@@ -27,7 +27,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>This is your new component - v3</Text>
+      <Text>This is your new component - v4</Text>
     </View>
   );
 }
